886ff8a97d2cf8879ea86d394c1e52a66dff2fc7
rubygitflow <nrubygitflow@gmail.com>
2024-02-17 17:10:06 +0600

    fix style - too-few-public-methods

--- survey/utils.py
+++ survey/utils.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
-class DataMixin:
+class DataMixin: # pylint: disable=too-few-public-methods
     def get_user_context(self, **kwargs):
         context = kwargs
         translate = {
